538a5c5309ab8b61a395eb49a51ea7a7a8b8878b
quran <qurandeyouxiang@qq.com>
2018-04-10 21:08:13 +0800

    add project

---
+++ libs/HelloLib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by quran on 2018/4/10.
+//
+
+#include "HelloLib.hpp"
---
+++ libs/HelloLib.hpp
@@ -0,0 +1,15 @@
+//
+// Created by quran on 2018/4/10.
+//
+
+#ifndef GOIM_CPP_HELLOLIB_HPP
+#define GOIM_CPP_HELLOLIB_HPP
+
+
+class HelloLib
+{
+
+};
+
+
+#endif //GOIM_CPP_HELLOLIB_HPP
